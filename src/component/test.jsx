ceb8a2bbd62a5b2b65e792ab0d17c71d5b6f79e7
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-05 22:46:29 +0300

    Test: Widget app.jsx

--- src/component/test.jsx
+++ src/component/test.jsx
@@ -1,4 +1,32 @@
+import React, { useEffect } from 'react';
+
 function Test() {
+  useEffect(() => {
+    // Verilen URL'den JavaScript dosyasını dinamik olarak yükle
+    const script = document.createElement('script');
+    script.src = 'https://widget-subemail.vercel.app/widget/index.js';
+    script.async = true;
+
+    // Script yüklendiğinde veya hata aldığınızda işlemleri yönetin
+    script.onload = () => {
+      // Yükleme başarılı, bu noktada dilediğiniz işlemleri yapabilirsiniz
+      console.log('Script yüklendi.');
+    };
+
+    script.onerror = () => {
+      // Yükleme hatası, hatayı işleyebilirsiniz
+      console.error('Script yüklenirken hata oluştu.');
+    };
+
+    // Script'i belirtilen URL'den ekleyin
+    document.body.appendChild(script);
+
+    // Component unmount edildiğinde script'i temizleyin
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
       TEST
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const widgetDivs = document.querySelectorAll('.nicoraynaud-finance-widget');
-
-widgetDivs.forEach(div => {
-    ReactDOM.render(
-      <React.StrictMode >
-        <App/>
-      </React.StrictMode>
-    );
-});
+ReactDOM.render(
+  <React.StrictMode >
+    <App/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
